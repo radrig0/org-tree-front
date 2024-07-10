6e786714483babef52379d9486f66ec66ed30f06
merokhin <erohin.mihail@gmail.com>
2024-07-10 03:06:46 +0200

    module org tree

--- src/app/_orgTree/OrgTree.tsx
+++ src/app/_orgTree/OrgTree.tsx
@@ -1,18 +1,29 @@
-import { FC } from "react";
-import { createUsers } from "@/app/_orgTree/testUsers";
-import { UserCard } from "@/app/_orgTree/UserCard/UserCard";
+'use client';
 
-const users = createUsers(20)
+import { FC, useContext, useEffect } from 'react';
+import { EmployeeCard } from './EmployeeCard/EmployeeCard';
+import { TreeRenderer } from './TreeRenderer/TreeRenderer';
+import styles from './orgTree.module.css';
+import { AddEmployeeForm } from './AddEmployeeForm/AddEmployeeForm';
+import { getList } from '@/actions/employeesActions';
+import { EmployeesContext } from '@/providers/EmployeesProvider';
 
 export const OrgTree: FC = () => {
+  const { employees, setEmployees } = useContext(EmployeesContext);
+
+  useEffect(() => {
+    const loadEmployees = async () => {
+      const fetchedList = await getList();
+      setEmployees(fetchedList);
+    };
+    loadEmployees();
+  }, [setEmployees]);
+
   return (
-    <>
-      <div>OrgTree</div>
-      <div>
-        {users.map(user => {
-          return <UserCard key={user.id} user={user}/>
-        })}
-      </div>
-    </>
-  )
-}
+    <div className={styles.wrapper}>
+      <AddEmployeeForm />
+      <div className={styles.title}>Org tree</div>
+      <TreeRenderer nodes={employees} renderComponent={EmployeeCard} />
+    </div>
+  );
+};
